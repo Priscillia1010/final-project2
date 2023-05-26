bdf29b314a59322569df0c448b0437dbb4c8ef4f
Priscillia1010 <priscilcandra.98@gmail.com>
2023-05-26 12:54:55 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/signin",
+    name: "signin",
+    component: () => import("../views/SigninView.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: () => import("../views/SignupView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
